87667673405b8b4c256f97d29c4b5313f80c5817
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-03-13 19:51:50 +0530

    serverless aws lamda

---
+++ hello.js
@@ -0,0 +1,11 @@
+//event has re obj -> req.body, req.headers
+module.exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({message: "Hello!"}),
+    };
+};
+  
